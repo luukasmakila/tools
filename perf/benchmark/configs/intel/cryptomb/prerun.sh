b9449d2e3ca8337ed0573dad8d7e162edb642230
Luukas Mäkilä <luukas.makila@intel.com>
2023-01-12 14:12:21 +0200

    rm intel configs

--- perf/benchmark/configs/intel/cryptomb/prerun.sh
+++
@@ -1,3 +0,0 @@
-echo 'Applying Envoy filter'
-kubectl apply -f "${HOME}/Desktop/tools/perf/istio-install/istio_telemetry/envoy-filter-cryptomb-stats.yaml"
-kubectl rollout restart deployment -n istio-system istio-ingressgateway
